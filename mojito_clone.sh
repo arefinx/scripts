c2a886f08c2e1f75ebee7b91dc1513b5d6f1c15f
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2023-08-23 10:34:24 +0600

    Initial commit

---
+++ mojito_clone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Define stuff
+ax=https://github.com/axsrog
+
+git clone $ax/device_xiaomi_mojito device/xiaomi/mojito
+git clone $ax/kernel_xiaomi_mojito kernel/xiaomi/mojito
+git clone $ax/vendor_xiaomi_mojito vendor/xiaomi/mojito
